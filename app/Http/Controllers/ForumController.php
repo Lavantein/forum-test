05b81a2229a720124d4d2580c1c4316bbae8d146
Lavantein <nico_afandi_cool@yahoo.co.id>
2016-08-31 00:06:31 +0700

    Update ForumController.php

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -47,27 +47,17 @@ class ForumController extends Controller
     {
         $this->validate($request, $this->rules);
 
+	// handle script and php tags
         $body = $request['body'];
         $doc = new \DOMDocument();
-
-        // load the HTML string we want to strip
         $doc->loadHTML($body);
-
-        // get all the script tags
         $script_tags = $doc->getElementsByTagName('script');
-
         $length = $script_tags->length;
-
-        // for each tag, remove it from the DOM
         for ($i = 0; $i < $length; $i++) {
           $script_tags->item($i)->parentNode->removeChild($script_tags->item($i));
         }
-
-        // get the HTML string back
         $body = $doc->saveHTML();
-
         $body = preg_replace('/^<\?php(.*)(\?>)?$/s', '$1', $body);
-
     	$post = new Post();
 
         $post->user_id = Auth::user()->id;
